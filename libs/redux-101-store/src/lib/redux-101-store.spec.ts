6052756eb656373087586f90b5fc24f1109f3996
Harsha Narayana <itsharshanarayana@gmail.com>
2022-09-04 15:45:50 -0400

    Spec files deleted

--- libs/redux-101-store/src/lib/redux-101-store.spec.ts
+++
@@ -1,7 +0,0 @@
-import { redux101Store } from './redux-101-store';
-
-describe('redux101Store', () => {
-  it('should work', () => {
-    expect(redux101Store()).toEqual('redux-101-store');
-  });
-});
