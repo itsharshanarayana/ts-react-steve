725a29b4801d99496f3e55d4eb5d3906bdef1aa7
Harsha Narayana <itsharshanarayana@gmail.com>
2022-09-08 06:41:31 -0400

    app:redux-basics - code refactor

--- libs/redux-basics-store/src/lib/actions/food-actions.ts
+++ libs/redux-basics-store/src/lib/actions/food-actions.ts
@@ -1,19 +1,20 @@
 import {FoodActionTypes} from "../types/food-action-types";
 import {Payload} from "../types/models";
 
-export interface UpdateFrozenAction {
+export interface Action {
+  payload: Payload;
+}
+
+export interface UpdateFrozenAction extends Action {
   type: FoodActionTypes.UPDATE_FROZEN_INV;
-  payload: Payload;
 }
 
-export interface UpdateProduceAction {
+export interface UpdateProduceAction extends Action {
   type: FoodActionTypes.UPDATE_PRODUCE_INV;
-  payload: Payload;
 }
 
-export interface UpdateMeatAction {
+export interface UpdateMeatAction extends Action {
   type: FoodActionTypes.UPDATE_MEAT_INV;
-  payload: Payload;
 }
 
 export type FoodAction = UpdateFrozenAction | UpdateProduceAction | UpdateMeatAction ;
